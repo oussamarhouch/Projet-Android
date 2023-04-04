65d271fea897c9caa493b44e351a57c7cffaa56b
fatima ezzahra <ouahmane.zahraa@gmail.com>
2023-04-04 22:44:15 +0000

    second commit

--- app/src/main/java/com/example/ideationnation/LoginActivity.kt
+++ app/src/main/java/com/example/ideationnation/LoginActivity.kt
@@ -27,7 +27,7 @@ class LoginActivity : AppCompatActivity() {
         changecolor(loginButton, signButton)
         val skipButton = findViewById<Button>(R.id.button3)
         skipButton.setOnClickListener {
-            val intent = Intent(this , ProfileActivity::class.java)
+            val intent = Intent(this , ProfilePicture::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/ideationnation/ProfilePicture.kt
+++ app/src/main/java/com/example/ideationnation/ProfilePicture.kt
@@ -1,10 +1,11 @@
 package com.example.ideationnation
-
+import android.util.Base64
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.*
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,54 +14,58 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ideationnation.R.id.confirm
+import java.io.ByteArrayOutputStream
+import java.util.*
+
 
 class ProfilePicture : AppCompatActivity() {
-    private val REQUEST_IMAGE_GALLERY = 100
-    private val REQUEST_IMAGE_CAPTURE = 101
-    private var mProfileImage: ImageView? = null
+
+    companion object {
+        const val REQUEST_IMAGE_GALLERY = 100
+        const val REQUEST_IMAGE_CAPTURE = 101
+    }
+
+    private var profileImageView: ImageView? = null
 
     @SuppressLint("CutPasteId", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_picture)
-        mProfileImage = findViewById(R.id.profile_image)
 
+        profileImageView = findViewById(R.id.profile_image)
         showImageSelectionDialog()
 
-        val addPhotoButton = findViewById<Button>(R.id.Add_photo)
+        findViewById<Button>(R.id.Add_photo).setOnClickListener { showImageSelectionDialog() }
 
-        addPhotoButton.setOnClickListener {
-            showImageSelectionDialog()
-        }
-
-        val skipButton = findViewById<Button>(R.id.skip)
-        skipButton.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
+        findViewById<Button>(R.id.skip).setOnClickListener {
+            startActivity(Intent(this, AccueilActivity::class.java))
         }
     }
 
     private fun showImageSelectionDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Choose an option")
-        builder.setItems(
-            arrayOf<CharSequence>("Gallery", "Camera"),
-            DialogInterface.OnClickListener { dialog, which ->
+        AlertDialog.Builder(this)
+            .setTitle("Choose an option")
+            .setItems(arrayOf<CharSequence>("Gallery", "Camera")) { dialog, which ->
                 when (which) {
-                    0 -> {
-                        val intent = Intent(Intent.ACTION_GET_CONTENT)
-                        intent.type = "image/*"
-                        startActivityForResult(intent, REQUEST_IMAGE_GALLERY)
-                    }
-                    1 -> {
-                        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        if (takePictureIntent.resolveActivity(packageManager) != null) {
-                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                        }
-                    }
+                    0 -> selectImageFromGallery()
+                    1 -> takeImageFromCamera()
                 }
-            })
-        builder.show()
+            }
+            .show()
+    }
+
+    private fun selectImageFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY)
+    }
+
+    private fun takeImageFromCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -70,25 +75,62 @@ class ProfilePicture : AppCompatActivity() {
                 REQUEST_IMAGE_GALLERY -> {
                     if (data != null) {
                         val imageUri = data.data
-                        mProfileImage?.setImageURI(imageUri)
-                        val confirmButton = findViewById<Button>(R.id.confirm)
-                            confirmButton?.visibility = View.VISIBLE
-                        confirmButton.setOnClickListener {
+                        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+                        val circularBitmap = getRoundedBitmap(bitmap)
 
+                        val sharedPrefs = getSharedPreferences("my_app_prefs", Context.MODE_PRIVATE)
+                        val editor = sharedPrefs.edit()
+                        editor.putString("profile_image_uri", imageUri.toString())
+                        editor.putString("profile_image_circular", encodeBitmap(circularBitmap))
+                        editor.apply()
+
+                        profileImageView?.setImageBitmap(circularBitmap)
+                        val confirmButton = findViewById<Button>(R.id.confirm)
+                        confirmButton?.visibility = View.VISIBLE
+                        confirmButton.setOnClickListener {
                             val intent = Intent(this, AccueilActivity::class.java)
                             startActivity(intent)
                         }
-
                     }
                 }
                 REQUEST_IMAGE_CAPTURE -> {
                     if (data != null && data.extras != null) {
                         val imageBitmap = data.extras!!.get("data") as Bitmap?
-                        mProfileImage?.setImageBitmap(imageBitmap)
-                        
+                        val circularBitmap = getRoundedBitmap(imageBitmap!!)
+                        val sharedPrefs = getSharedPreferences("my_app_prefs", Context.MODE_PRIVATE)
+                        val editor = sharedPrefs.edit()
+                        editor.putString("profile_image_circular", encodeBitmap(circularBitmap))
+                        editor.apply()
+                        profileImageView?.setImageBitmap(circularBitmap)
+
                     }
                 }
             }
         }
     }
+
+    private fun encodeBitmap(bitmap: Bitmap): String {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream)
+        val byteArray = outputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
+    private fun getRoundedBitmap(bitmap: Bitmap): Bitmap {
+        val output = Bitmap.createBitmap(bitmap.width * 2, bitmap.height * 2, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+
+        val radius = bitmap.width / 2.toFloat() *2
+
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        canvas.drawCircle(radius, radius, radius, paint)
+
+        return output
+    }
+
+
+
 }
