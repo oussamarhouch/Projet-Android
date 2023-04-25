51e1b25ad7b6a8c580ed6e282c68c0885849e6f5
Mouad Toubil <pogchampouu@gmail.com>
2023-04-25 07:25:54 +0100

    Add activity: ajout de l'envoie de userid avec l'idée. :)

--- app/src/main/java/com/example/ideationnation/AddActivity.kt
+++ app/src/main/java/com/example/ideationnation/AddActivity.kt
@@ -1,19 +1,31 @@
 package com.example.ideationnation
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
 import android.widget.Button
 import android.widget.Toast
 import com.example.ideationnation.R
 import com.example.ideationnation.databinding.ActivityAddIdeaBinding
 import com.example.ideationnation.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.storage.StorageReference
 
 class AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddIdeaBinding
+    private lateinit var auth : FirebaseAuth
     private lateinit var database: DatabaseReference
+    private lateinit var storageReference: StorageReference
+    private lateinit var dialog: Dialog
+    private lateinit var user: User
+    private lateinit var uid :String
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +37,13 @@ class AddActivity : AppCompatActivity() {
             val myIdea = binding.addIdea.text.toString()
             val title=binding.addTitle.text.toString()
 
+            //ajout de uid
+            auth = FirebaseAuth.getInstance()
+            uid = auth.currentUser?.uid.toString()
+
+
             database=FirebaseDatabase.getInstance("https://ideation-nation-f87ec-default-rtdb.europe-west1.firebasedatabase.app").getReference("myIdeas")
-            val idea= Idea(title, myIdea)
+            val idea= Idea(title, myIdea,uid)
             database.child(title).setValue(idea).addOnSuccessListener {
 
                 binding.addIdea.text.clear()
